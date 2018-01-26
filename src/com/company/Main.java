72924be2fa396843029400b260c9e0b0c5de15b5
Alex Keith <alexmkeith13@gmail.com>
2018-01-26 15:27:30 -0500

    Added loops

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,8 +12,27 @@ public class Main {
 //        5. allow the user play again if they would like to
 
         Scanner input = new Scanner(System.in);
+        int numberOfGuesses = 0;
 
-        System.out.println("Enter your guess:");
-        int userGuess = input.nextInt();
+        final int MAX_GUESS_COUNT = 4;
+
+        int generatedNumber = (int) Math.ceil(Math.random() * 100);
+
+            System.out.println("Enter your guess:");
+            int userGuess = input.nextInt();
+
+            do {
+                if (userGuess > generatedNumber) {
+                    System.out.println("Your number is too high!");
+                    numberOfGuesses++;
+                } else if (userGuess < generatedNumber) {
+                    System.out.println("Your number is too low!");
+                    numberOfGuesses++;
+                } else if (userGuess == generatedNumber) {
+                    System.out.println("Your number is correct!");
+                } else if (numberOfGuesses == MAX_GUESS_COUNT) {
+                    System.out.println("Sorry, you have ran out of chances. The correct number was " + generatedNumber);
+                }
+            } while (numberOfGuesses <= 4);
+        }
     }
-}
