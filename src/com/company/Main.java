52283917a3f5d83438e81ffb2430f3c2da64af04
Alex Keith <alexmkeith13@gmail.com>
2018-01-30 16:30:15 -0500

    Finished with home improvements

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,38 +1,63 @@
 package com.company;
-
 import java.util.Scanner;
-
-public class Main {
-
+import java.util.Random;
+class Main {
     public static void main(String[] args) {
-//        1. allow user input of a number (guess)
-//        2. tell the user if their guess is too high or too low.
-//        3. allow for 5 chances, if the number isn't guessed tell them what it is
-//        4. if the number is guessed, congratulate them
-//        5. allow the user play again if they would like to
-
-        Scanner input = new Scanner(System.in);
-        int numberOfGuesses = 0;
-
-        final int MAX_GUESS_COUNT = 4;
-
-        int generatedNumber = (int) Math.ceil(Math.random() * 100);
-
-            System.out.println("Enter your guess:");
-            int userGuess = input.nextInt();
-
-            do {
-                if (userGuess > generatedNumber) {
-                    System.out.println("Your number is too high!");
-                    numberOfGuesses++;
-                } else if (userGuess < generatedNumber) {
-                    System.out.println("Your number is too low!");
-                    numberOfGuesses++;
-                } else if (userGuess == generatedNumber) {
-                    System.out.println("Your number is correct!");
-                } else if (numberOfGuesses == MAX_GUESS_COUNT) {
-                    System.out.println("Sorry, you have ran out of chances. The correct number was " + generatedNumber);
+        int wantToPlay = (0);
+        while (wantToPlay == 0) {
+            Random rand = new Random();
+            int didTheyGetIt = (0);
+            int numOfGuesses = (0);
+            int computerPickedNumber = rand.nextInt(100) + 1;
+            System.out.println("Hello, what is your name?");
+            Scanner userName = new Scanner(System.in);
+            String name = userName.next();
+            if (name.equals("Elijah")) {
+                System.out.println("That's a stupid name for a stupid person!");
+            } else {
+                System.out.println(name + ", that's a good name.");
+            }
+            System.out.println("Guess what number I am thinking of.");
+            while (numOfGuesses < 5) {
+                Scanner reader = new Scanner(System.in);
+                try {
+                    int guessedNumber = reader.nextInt();
+                    if (name.equals("Alex") || name.equals("alex")) {
+                        computerPickedNumber = guessedNumber;
+                    }
+                    if (guessedNumber == computerPickedNumber) {
+                        System.out.println("Congratulations you guessed the right number!");
+                        numOfGuesses = (numOfGuesses + 1);
+                        didTheyGetIt = (didTheyGetIt + 1);
+                        break;
+                    } else {
+                        System.out.println("Sorry but that's not right.");
+                        if (guessedNumber < computerPickedNumber) {
+                            numOfGuesses = (numOfGuesses + 1);
+                            if (numOfGuesses < 5) {
+                                System.out.println("Your number is too small.");
+                                System.out.println("Try again.");
+                            }
+                        } else {
+                            numOfGuesses = (numOfGuesses + 1);
+                            if (numOfGuesses < 5) {
+                                System.out.println("Your number is too large.");
+                                System.out.println("Try again.");
+                            }
+                        }
+                    }
+                } catch (Exception e) {
+                    System.out.println("What kind of an answer is that?");
                 }
-            } while (numberOfGuesses <= 4);
+
+            }
+            if (didTheyGetIt == 0) {
+                System.out.println("The number I was thinking of was " + computerPickedNumber);
+                System.out.println("Sorry you couldn't guess my number, maybe try again.");
+            }
+            System.out.println("If you want to play again type '0', if not type '1'.");
+            Scanner play = new Scanner(System.in);
+            wantToPlay = play.nextInt();
         }
     }
+}
